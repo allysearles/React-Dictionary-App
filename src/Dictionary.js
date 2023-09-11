e290c682c5d22e6449ba1f768277217b2768062c
allysearles <alyssa.searles001@mymdc.net>
2023-09-11 18:35:33 -0400

    Home-Page: Added Search Engine API Integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState("");
 
+	function handleResponse(response) {
+		console.log(response.data[0]);
+	}
 	function search(event) {
 		event.preventDefault();
-		alert(`Searching for ${keyword}`);
+
+		//Documentation: https://dictionaryapi.dev/
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 	function handleKeywordChange(event) {
 		setKeyword(event.target.value);
 	}
 	return (
 		<div className="Dictionary">
-			<form onSubmit={search}>
+			<form id="search-form" onSubmit={search}>
 				<div className="row">
 					<div className="col-10">
-						{" "}
 						<input
+							id="search-bar"
 							type="search"
 							placeholder="Search . . ."
 							className="form-control"
