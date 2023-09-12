d05a698ba6f21d6bc449e5b980d4e7228e724f65
allysearles <alyssa.searles001@mymdc.net>
2023-09-11 20:54:53 -0400

    Home-Page: Rendered Meanings For Dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,46 +1,63 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
-export default function Dictionary() {
-	let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+	let [keyword, setKeyword] = useState(props.defaultkeyword);
+	let [results, setResults] = useState("");
+	let [loaded, setLoaded] = useState(false);
 
 	function handleResponse(response) {
-		console.log(response.data[0]);
+		setResults(response.data);
 	}
-	function search(event) {
-		event.preventDefault();
-
+	function search() {
 		//Documentation: https://dictionaryapi.dev/
-		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=bf3ac0311d9084536tao67043b4313ef`;
 		axios.get(apiUrl).then(handleResponse);
+	}
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
 	}
 	function handleKeywordChange(event) {
 		setKeyword(event.target.value);
 	}
-	return (
-		<div className="Dictionary">
-			<form id="search-form" onSubmit={search}>
-				<div className="row">
-					<div className="col-10">
-						<input
-							id="search-bar"
-							type="search"
-							placeholder="Search . . ."
-							className="form-control"
-							onChange={handleKeywordChange}
-						/>
+	function load() {
+		setLoaded(true);
+		search();
+	}
+	if (loaded) {
+		return (
+			<div className="Dictionary">
+				<form id="search-form" onSubmit={handleSubmit}>
+					<div className="row">
+						<div className="col-10">
+							<input
+								type="search"
+								placeholder="Search . . ."
+								className="form-control"
+								onChange={handleKeywordChange}
+								defaultValue={props.defaultkeyword}
+								autoFocus={true}
+							/>
+						</div>
+						<div className="col-2">
+							<input
+								type="submit"
+								value="Search"
+								className="btn btn-primary w-100"
+								id="search-button"
+							/>
+						</div>
 					</div>
-					<div className="col-2">
-						<input
-							type="submit"
-							value="Search"
-							className="btn btn-primary w-100"
-							id="search-button"
-						/>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+				</form>
+				<Results results={results} />
+			</div>
+		);
+	} else {
+		load();
+		return "Searching...";
+	}
 }
---
+++ src/Meaning.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Synonyms from "./Synonyms";
+
+export default function Meaning(props) {
+	return (
+		<div className="Meaning">
+			<h3>{props.meaning.partOfSpeech}</h3>
+			<div className="definition">{props.meaning.definition}</div>
+			<div className="example">{props.meaning.example}</div>
+			<Synonyms synonyms={props.meaning.synonyms} />
+		</div>
+	);
+}
