a7d3bfb8f5dbe4a1f45637bd4b735535043a1af9
allysearles <alyssa.searles001@mymdc.net>
2023-09-11 21:15:23 -0400

    Updated Dictionary API Integration

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,10 +4,12 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
 	return (
 		<div className="Meaning">
-			<h3>{props.meaning.partOfSpeech}</h3>
-			<div className="definition">{props.meaning.definition}</div>
-			<div className="example">{props.meaning.example}</div>
-			<Synonyms synonyms={props.meaning.synonyms} />
+			<section>
+				<h3>{props.meaning.partOfSpeech}</h3>
+				<div className="definition">{props.meaning.definition}</div>
+				<div className="example">{props.meaning.example}</div>
+				<Synonyms synonyms={props.meaning.synonyms} />
+			</section>
 		</div>
 	);
 }
